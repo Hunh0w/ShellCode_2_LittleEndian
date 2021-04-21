1fecf48998df1c48494bda2335a09e5ead487a62
Hunh0w <42644966+Hunh0w@users.noreply.github.com>
2021-04-21 21:35:12 +0000

    Create lendian.sh

---
+++ lendian.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function stop() {
+    exit 0
+}
+
+if [[ -z $1 ]]; then
+    read arg
+else
+    arg=$1
+fi
+var=${arg//[\\x]/ }
+output=""
+for i in $var; do
+    output="\\x$i$output"
+done
+echo $output
